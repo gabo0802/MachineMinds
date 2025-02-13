be8e3b0d512ae332c1563d4860278dac23292450
vanbest-glitch <58440254+vanbest-glitch@users.noreply.github.com>
2025-02-13 18:38:03 -0500

    Added ability to destroy walls (toggleable)

--- MachineMindsUnity/Assets/Scripts/WallBehavior.cs
+++ MachineMindsUnity/Assets/Scripts/WallBehavior.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 
-public class WallBehavior : MonoBehaviour
-{
+public class WallBehavior : MonoBehaviour{
+    public bool isDestroyable = false;
+
     void OnBulletHit(string bulletType){
-    
+        if(isDestroyable){
+            Destroy(gameObject);
+        }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
