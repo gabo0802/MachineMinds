135ef5e36780301220b1ff5e0e1a060ecbed71ca
vanbest-glitch <58440254+vanbest-glitch@users.noreply.github.com>
2025-02-13 12:31:39 -0500

    can actually quit game

--- MachineMindsUnity/Assets/Scripts/GameManager.cs
+++ MachineMindsUnity/Assets/Scripts/GameManager.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor; //only for testing, remove when building
+
 public class GameManager : MonoBehaviour
 {
-    public void PlayGame()
-    {
+    public void PlayGame(){
         SceneManager.LoadScene("Scenes/Levels/Level1", LoadSceneMode.Single);
     }
 
-    public void QuitGame()
-    {
-        Debug.Log("Quit Game");
+    public void QuitGame(){
+        Application.Quit();
+        EditorApplication.ExitPlaymode(); //only for testing, remove when building
     }
 }
