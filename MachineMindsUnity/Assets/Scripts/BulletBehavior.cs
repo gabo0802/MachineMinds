e8aea2a610b0a01365bd5ae31e86e8b5ec2b58a4
gabo0802 <gabriel.castejon0802@gmail.com>
2025-02-19 15:34:00 -0500

    Fixed bullet bouncing angle to be more natural

--- MachineMindsUnity/Assets/Scripts/BulletBehavior.cs
+++ MachineMindsUnity/Assets/Scripts/BulletBehavior.cs
@@ -6,11 +6,6 @@ public class BulletBehavior : MonoBehaviour{
     // -Gabe: Maybe nerf or allow the GameObject set this value, we could have different enemy types
     // that have different bullet speeds.
     public float bulletSpeed = 1f; 
-    
-    // - Gabe: This bounce angle should not be constant??? 
-    // It should be based on the angle at which the bullet is hitting, 
-    // so calcuated once it connects with collision.
-    public float bulletBounceAngle = 45f; //do we really want to have bouncy bullets? (Gabe - yes I think so but diff implementation)
     public float bulletLifeTime = 10f; // -Gabe: Still think that this should be a bounce count instead of lifetime.
     private float bulletLifeTimer = 0f;
     public float bulletDetectRange = 0.25f;
@@ -30,19 +25,34 @@ public class BulletBehavior : MonoBehaviour{
         Debug.Log(gameObject.name + " got hit be explosion");
     }
 
+    void BounceBullet(RaycastHit2D hit, Vector2 moveDirection) {
+        transform.position = hit.point + hit.normal * 0.05f; // Move slightly away
+        Vector2 newDirection = Vector2.Reflect(moveDirection, hit.normal);
+        float angle = Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        rb.linearVelocity = newDirection * bulletSpeed; // Apply new velocity
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update(){
-        //Constantly move
-        rb.linearVelocity = transform.up * bulletSpeed; 
+    void Update() {
 
-        //Object Hit Detection:
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + ((bulletDetectRange / 2) * transform.up), transform.up, (bulletDetectRange / 2)); //in theory should work, if not need to change where looking (might be transform.up)
-        if(hit){       
+    }  
+    
+    // Use FixedUpdate for physics calculations
+    void FixedUpdate()
+    {
+        Vector2 moveDirection = transform.up;
+        float moveDistance = bulletSpeed * Time.fixedDeltaTime;
+
+        // Raycast ahead to check if a collision will happen before moving
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, moveDirection, moveDistance);
+        if (hit)
+        {
             hit.transform.SendMessageUpwards("OnBulletHit", gameObject);
             
             if(isExplody){
@@ -52,21 +62,31 @@ public class BulletBehavior : MonoBehaviour{
                     currentExplodedObject.transform.SendMessageUpwards("OnExplosionHit");
                 }
             }
-
-            if(isBouncy){
-                transform.Rotate(new Vector3(0, 0, bulletBounceAngle));
-            }else{
+            if (isBouncy)
+            {
+                BounceBullet(hit, moveDirection);
+            }
+            else
+            {
                 Destroy(gameObject);
             }
+            
+        }
+        else
+        {
+            rb.linearVelocity = moveDirection * bulletSpeed; // Normal movement
         }
 
-        //Object Lifetime:
-        if(bulletLifeTimer >= bulletLifeTime){
+        // Bullet Lifetime
+        if (bulletLifeTimer >= bulletLifeTime)
+        {
             Destroy(gameObject);
-        }else{
+        }
+        else
+        {
             bulletLifeTimer += Time.deltaTime;
         }
-
-        
     }
+
+
 }
