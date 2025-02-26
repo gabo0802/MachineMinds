1e42ae157fc00b07c1bfbed9895dc92776008c7a
vanbest-glitch <58440254+vanbest-glitch@users.noreply.github.com>
2025-02-25 23:25:43 -0500

    EndScreen UI Functionality added

--- MachineMindsUnity/Assets/Scripts/EndScreenScript.cs
+++ MachineMindsUnity/Assets/Scripts/EndScreenScript.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
 //using UnityEditor; //only for testing, remove when building
 
-public class EndScreenScript : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+public class EndScreenScript : MonoBehaviour{
+    public TMPro.TextMeshProUGUI finalPointsUI = null;
+    private string saveFilePath = Application.dataPath + "/Resources/GameState.save";
+
     public void MainMenuButton(){
         SceneManager.LoadScene("Scenes/MainMenu", LoadSceneMode.Single);
     }
@@ -13,5 +15,22 @@ public class EndScreenScript : MonoBehaviour
     public void QuitGame(){
         Application.Quit();
         //EditorApplication.ExitPlaymode(); //only for testing, remove when building
+    }
+
+    void Start(){
+        if (File.Exists(saveFilePath)){
+            string saveFileData = "";
+            using (StreamReader saveFile = File.OpenText(saveFilePath)){
+                string currentLine;
+                while ((currentLine = saveFile.ReadLine()) != null){
+                    saveFileData += currentLine + "\n";
+                }
+            }
+
+            string[] fileArray = saveFileData.Split('\n');
+            string totalPoints = fileArray[1];
+
+            finalPointsUI.text = "Final Score: " + totalPoints;
+         }
     }
 }
