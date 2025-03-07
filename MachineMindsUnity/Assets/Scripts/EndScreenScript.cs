d4cf50707c1fb99b76e895fccb6d303070abf011
gabo0802 <gabriel.castejon0802@gmail.com>
2025-03-07 12:54:16 -0500

    Fix game end for editor, add firebase sample code.

--- MachineMindsUnity/Assets/Scripts/EndScreenScript.cs
+++ MachineMindsUnity/Assets/Scripts/EndScreenScript.cs
@@ -1,30 +1,46 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-//using UnityEditor; //only for testing, remove when building
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-public class EndScreenScript : MonoBehaviour{
+public class EndScreenScript : MonoBehaviour
+{
     public TMPro.TextMeshProUGUI finalPointsUI = null;
     private const string SAVE_KEY = "GameState";
 
-    public void MainMenuButton(){
-        SceneManager.LoadScene("Scenes/MainMenu", LoadSceneMode.Single);
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 
-    public void QuitGame(){
-        Application.Quit();
-        //EditorApplication.ExitPlaymode(); //only for testing, remove when building
+    public void QuitGame()
+    {
+        if (Application.isEditor)
+        {
+            Debug.Log("Quitting game in editor");
+            EditorApplication.ExitPlaymode();
+        }
+        else
+        {
+            Debug.Log("Quitting game in build");
+            Application.Quit();
+        }
     }
 
-    void Start(){
-        if (WebGLSaveSystem.FileExists(SAVE_KEY)){
+    void Start()
+    {
+        if (WebGLSaveSystem.FileExists(SAVE_KEY))
+        {
             string saveFileData = WebGLSaveSystem.ReadAllText(SAVE_KEY);
-            
+
             string[] fileArray = saveFileData.Split('\n');
             string totalPoints = fileArray[1];
-            
+
             finalPointsUI.text = "Final Score: " + totalPoints;
             //test comment
         }
     }
 }
+
