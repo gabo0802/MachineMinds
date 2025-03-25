1d000552e826607d90ad76bb6719732f4a713d03
vanbest-glitch <58440254+vanbest-glitch@users.noreply.github.com>
2025-03-25 11:57:57 -0400

    script working now

--- MachineMindsUnity/AI/run_model.py
+++ MachineMindsUnity/AI/run_model.py
@@ -6,17 +6,23 @@ import sys
 def get_ai_model_value(x_data):
     dt = DecisionTreeClassifier()
     dt = load('filename.joblib')
-    return dt.predict(x_data)
+    return dt.predict(x_data)[0]
 
 if __name__ == "__main__":
-    print(sys.argv)
+    #command syntax: 'python -W ignore run_model.py currentDifficulty currentPlayerLives levelsBeat totalEnemiesKilled totalPoints
+    #command example: 'python -W ignore run_model.py 1 3 3 2 44.75 11 0'
 
-    x_value = pd.DataFrame()
-    x_value['currentDifficulty'] = sys.argv[0]
-    x_value['currentPlayerLives'] = sys.argv[1]
-    x_value['levelsBeat'] = sys.argv[2]
-    x_value['playerLifeTimer'] = sys.argv[3]
-    x_value['totalEnemiesKilled'] = sys.argv[4]
-    x_value['totalPoints'] = sys.argv[5]
+    #print(sys.argv)    
+    if len(sys.argv) < 6:
+        print("missing arguements")
+    else:
+        x_value = pd.DataFrame()
+        x_value['currentDifficulty'] = [sys.argv[1]]
+        x_value['currentPlayerLives'] = [sys.argv[2]]
+        x_value['levelsBeat'] = [sys.argv[3]]
+        x_value['playerLifeTimer'] = [sys.argv[4]]
+        x_value['totalEnemiesKilled'] = [sys.argv[5]]
+        x_value['totalPoints'] = [sys.argv[6]]
 
-    print(get_ai_model_value(x_value))
+        #print(x_value)
+        print(get_ai_model_value(x_value))
