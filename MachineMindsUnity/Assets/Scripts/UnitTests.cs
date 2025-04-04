8004d7be0872352b047d6d1d16e571c014d27070
vanbest-glitch <58440254+vanbest-glitch@users.noreply.github.com>
2025-04-04 16:41:34 -0400

    Unit Tests additions (gabe will be unhappy)

--- MachineMindsUnity/Assets/Scripts/UnitTests.cs
+++ MachineMindsUnity/Assets/Scripts/UnitTests.cs
@@ -1,7 +1,6 @@
+using UnityEngine;
 using System.Diagnostics;
 using System;
-using UnityEngine;
-
 using static EnemyBehavior;
 
 public class UnitTests : MonoBehaviour
@@ -9,6 +8,16 @@ public class UnitTests : MonoBehaviour
     public void Test_One(int newDifficultyLevel){
         EnemyBehavior e = new EnemyBehavior();
         e.SetDifficultyLevel(newDifficultyLevel);
-        System.Diagnostics.Debug.Assert(e.currentDifficulty == newDifficultyLevel);
+        //System.Diagnostics.Debug.Assert(e.currentDifficulty == newDifficultyLevel);
+        UnityEngine.Debug.Assert(e.currentDifficulty == newDifficultyLevel);
+    }
+
+    static void Main(string[] args){
+        UnitTests u = new UnitTests();
+        u.Test_One(1);
+    }
+
+    void Start(){
+        Test_One(5);
     }
 }
