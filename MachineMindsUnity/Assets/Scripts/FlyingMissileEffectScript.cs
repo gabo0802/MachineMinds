b744f9b2b799429d1e20a8714e54ccaace4c2ec8
gabo0802 <gabriel.castejon0802@gmail.com>
2025-04-08 16:46:26 -0400

    debugged some edge cases.

--- MachineMindsUnity/Assets/Scripts/FlyingMissileEffectScript.cs
+++ MachineMindsUnity/Assets/Scripts/FlyingMissileEffectScript.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class FlyingMissileEffectScript : MonoBehaviour{
+public class FlyingMissileEffectScript : MonoBehaviour
+{
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Vector3 targetPosition = Vector3.zero;
     private float totalDistanceToTarget;
@@ -11,14 +12,15 @@ public class FlyingMissileEffectScript : MonoBehaviour{
     private GameObject currentReticle;
     public GameObject explosionObject;
     public float explosionRadius = 1f;
-    
+
     private float worldScale = 2.0f;
     private float angleChangeScale = 10f;
     private float sizeChangeScale = 0.02f;
     private float speedScale = 0.05f;
-    
 
-    void setTargetPosition(Vector3 newPosition){
+
+    void setTargetPosition(Vector3 newPosition)
+    {
         targetPosition = newPosition;
 
         currentReticle = Instantiate(targetReticle, targetPosition, new Quaternion(0, 0, 0, 0));
@@ -29,16 +31,19 @@ public class FlyingMissileEffectScript : MonoBehaviour{
         sizeChangeScale /= totalDistanceToTarget;
     }
 
-    void setSpeedScale(float newSpeed){
+    void setSpeedScale(float newSpeed)
+    {
         speedScale = newSpeed;
     }
 
-    void setAngleChangeSpeed(float newSpeed){
+    void setAngleChangeSpeed(float newSpeed)
+    {
         angleChangeScale = newSpeed;
         angleChangeScale /= totalDistanceToTarget;
     }
 
-    void Start(){
+    void Start()
+    {
         /*targetPosition = new Vector3(Random.Range(-20, 0), Random.Range(-5, 5), 0);
         //targetPosition = (Vector3)((Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition));
         currentReticle = Instantiate(targetReticle, targetPosition, new Quaternion(0, 0, 0, 0));
@@ -49,7 +54,8 @@ public class FlyingMissileEffectScript : MonoBehaviour{
         sizeChangeScale /= totalDistanceToTarget;*/
     }
 
-    void EndOfPath(){
+    void EndOfPath()
+    {
         Collider2D[] allExplodedObjects = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
         GameObject currentExplosionObject = (GameObject)Instantiate(explosionObject, transform.position, transform.rotation);
         currentExplosionObject.transform.localScale = new Vector3(explosionRadius * worldScale, explosionRadius * worldScale, 1);
@@ -58,35 +64,47 @@ public class FlyingMissileEffectScript : MonoBehaviour{
         Debug.DrawLine(transform.position - new Vector3(0, explosionRadius, 0), transform.position + new Vector3(0, explosionRadius, 0), Color.red, 2.5f);
 
 
-        foreach (Collider2D currentExplodedObject in allExplodedObjects){
+        foreach (Collider2D currentExplodedObject in allExplodedObjects)
+        {
             currentExplodedObject.transform.SendMessageUpwards("OnExplosionHit");
         }
     }
 
+    void OnExplosionHit()
+    {
+        return;
+    }
+
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
         currentDistanceFromTarget = Vector3.Distance(transform.position, targetPosition);
         float sizeChangeScaleWeighted = sizeChangeScale * Time.deltaTime * 500f;
         float speedScaleWeighted = speedScale * Time.deltaTime * 500f;
         float angleChangeScaleWeighted = angleChangeScale * Time.deltaTime * 500f;
 
-        if(currentDistanceFromTarget > totalDistanceToTarget / 2){
-            missileObject.transform.localScale = new Vector3(missileObject.transform.localScale.x + sizeChangeScaleWeighted, 
+        if (currentDistanceFromTarget > totalDistanceToTarget / 2)
+        {
+            missileObject.transform.localScale = new Vector3(missileObject.transform.localScale.x + sizeChangeScaleWeighted,
                                                 missileObject.transform.localScale.y + sizeChangeScaleWeighted,
                                                 missileObject.transform.localScale.z + sizeChangeScaleWeighted);
 
             transform.position += transform.up * speedScaleWeighted;
-        }else if(currentDistanceFromTarget > 0.1f){
+        }
+        else if (currentDistanceFromTarget > 0.1f)
+        {
             missileObject.transform.Rotate(angleChangeScaleWeighted, 0, 0);
 
-            missileObject.transform.localScale = new Vector3(missileObject.transform.localScale.x - sizeChangeScaleWeighted, 
+            missileObject.transform.localScale = new Vector3(missileObject.transform.localScale.x - sizeChangeScaleWeighted,
                                                 missileObject.transform.localScale.y - sizeChangeScaleWeighted,
                                                 missileObject.transform.localScale.z - sizeChangeScaleWeighted);
             transform.position += transform.up * speedScaleWeighted;
-        }else{
-                EndOfPath();
-                Destroy(currentReticle);
-                Destroy(gameObject);
+        }
+        else
+        {
+            EndOfPath();
+            Destroy(currentReticle);
+            Destroy(gameObject);
         }
     }
 }
--- MachineMindsUnity/Assets/Scripts/ShieldScript.cs
+++ MachineMindsUnity/Assets/Scripts/ShieldScript.cs
@@ -4,28 +4,34 @@ public class ShieldScript : MonoBehaviour
 {
     public AudioSource soundEffectSoundPlayer;
 
-    private void volumeAdjustments(){
-        if (soundEffectSoundPlayer && PlayerPrefs.HasKey("SoundEffectVolume")){
+    private void volumeAdjustments()
+    {
+        if (soundEffectSoundPlayer && PlayerPrefs.HasKey("SoundEffectVolume"))
+        {
             soundEffectSoundPlayer.volume = PlayerPrefs.GetFloat("SoundEffectVolume");
         }
     }
-    
+
     void OnExplosionHit()
     {
+        return;
     }
 
     void OnBulletHit(GameObject bullet)
     {
-        Destroy(bullet);   
+        Destroy(bullet);
     }
 
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.transform.name.ToLower().Contains("bullet")){
-           Destroy(other.gameObject);
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.name.ToLower().Contains("bullet"))
+        {
+            Destroy(other.gameObject);
         }
     }
 
-    void Update(){
+    void Update()
+    {
         volumeAdjustments();
     }
 }
