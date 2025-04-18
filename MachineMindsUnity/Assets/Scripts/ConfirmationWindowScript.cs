1a34754741916538bfacc9f2f1c8be4909d1740e
Benjamin Bryant <100496281+Bencb03@users.noreply.github.com>
2025-04-18 12:56:04 -0400

    added documentation

--- MachineMindsUnity/Assets/Scripts/ConfirmationWindowScript.cs
+++ MachineMindsUnity/Assets/Scripts/ConfirmationWindowScript.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+
+/// <summary>
+/// Simple confirmation menu handler for destroying the menu on back.
+/// </summary>
 public class ConfirmationMenuScriptMenuScript : MonoBehaviour
-{    
-    public void onBackButtonPress(){
+{
+    /// <summary>
+    /// Called when the back button is pressed; closes this confirmation menu.
+    /// </summary>
+    public void onBackButtonPress()
+    {
         Destroy(gameObject);
     }
 }
