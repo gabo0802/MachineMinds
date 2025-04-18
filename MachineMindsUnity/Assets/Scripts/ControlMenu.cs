9e38cdd70c1e851e749ab4d9ce32298555314b12
Benjamin Bryant <100496281+Bencb03@users.noreply.github.com>
2025-04-18 12:57:34 -0400

    Documentation added

--- MachineMindsUnity/Assets/Scripts/ControlMenu.cs
+++ MachineMindsUnity/Assets/Scripts/ControlMenu.cs
@@ -6,18 +6,12 @@ using System;
 using UnityEditor;
 #endif
 
+/// <summary>
+/// Manages the controls settings menu: displays current key bindings,
+/// allows remapping keys, resetting to defaults, and handles confirmation dialogs.
+/// </summary>
 public class ControlsMenuScriptMenuScript : MonoBehaviour
 {
-    /*
-        KeyMovementUp
-        KeyMovementDown
-        KeyMovementLeft
-        KeyMovementRight
-        KeyBoost
-        KeyShoot
-        KeyPause
-    */
-
     private int currentKeyIndex = -1;
     public TMPro.TextMeshProUGUI[] keyChangeInputs;
     public string[] playerPrefsKeyMatch;
@@ -25,63 +19,96 @@ public class ControlsMenuScriptMenuScript : MonoBehaviour
     public GameObject confirmationWindowObject;
     private GameObject currentConfirmationWindowObject;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start(){
-        for(int i = 0; i < keyChangeInputs.Length; i++){
-            if(PlayerPrefs.HasKey(playerPrefsKeyMatch[i])){
+    /// <summary>
+    /// Initializes UI fields with saved keybindings or defaults on start.
+    /// </summary>
+    void Start()
+    {
+        for (int i = 0; i < keyChangeInputs.Length; i++)
+        {
+            if (PlayerPrefs.HasKey(playerPrefsKeyMatch[i]))
+            {
                 string keyCodeString = PlayerPrefs.GetString(playerPrefsKeyMatch[i]);
-                if(i != 4 && i != 5 && keyCodeString.Length > 3){
+                if (i != 4 && i != 5 && keyCodeString.Length > 3)
+                {
                     keyCodeString = keyCodeString.Substring(0, 3);
                 }
                 keyChangeInputs[i].text = keyCodeString;
-            }else{
+            }
+            else
+            {
                 keyChangeInputs[i].text = defaultKeyCode[i];
             }
         }
     }
 
-    public void onBackButtonPress(){
+    /// <summary>
+    /// Closes the controls menu when the back button is pressed.
+    /// </summary>
+    public void onBackButtonPress()
+    {
         Destroy(gameObject);
     }
 
-    public void onKeyChangePress(int keyIndex){
+    /// <summary>
+    /// Marks a key input field as active for remapping.
+    /// </summary>
+    public void onKeyChangePress(int keyIndex)
+    {
         keyChangeInputs[keyIndex].text = "...";
         currentKeyIndex = keyIndex;
     }
 
-    public void resetAllKeys(){
-        for(int i = 0; i < keyChangeInputs.Length; i++){
-            if(PlayerPrefs.HasKey(playerPrefsKeyMatch[i])){
+    /// <summary>
+    /// Resets all keybindings to their default values and updates the UI.
+    /// </summary>
+    public void resetAllKeys()
+    {
+        for (int i = 0; i < keyChangeInputs.Length; i++)
+        {
+            if (PlayerPrefs.HasKey(playerPrefsKeyMatch[i]))
                 PlayerPrefs.DeleteKey(playerPrefsKeyMatch[i]);
-            }
-            
+
             keyChangeInputs[i].text = defaultKeyCode[i];
         }
     }
 
-    // Update is called once per frame
-
-    void OnGUI(){
+    /// <summary>
+    /// Captures and saves a new keybinding when a key is pressed after initiating remap.
+    /// Displays a confirmation dialog upon success.
+    /// </summary>
+    void OnGUI()
+    {
         Event e = Event.current;
-        if (!currentConfirmationWindowObject && currentKeyIndex != -1 && e.isKey){   
-             try{
-                string keyCodeString = Event.current.keyCode + "";
+        if (!currentConfirmationWindowObject && currentKeyIndex != -1 && e.isKey)
+        {
+            try
+            {
+                string keyCodeString = e.keyCode.ToString();
                 PlayerPrefs.SetString(playerPrefsKeyMatch[currentKeyIndex], keyCodeString);
-                if(currentKeyIndex != 4 && currentKeyIndex != 5 && keyCodeString.Length > 3){
+                if (currentKeyIndex != 4 && currentKeyIndex != 5 && keyCodeString.Length > 3)
                     keyCodeString = keyCodeString.Substring(0, 3);
-                }
+
                 keyChangeInputs[currentKeyIndex].text = keyCodeString;
                 currentKeyIndex = -1;
-                currentConfirmationWindowObject = (GameObject) Instantiate(confirmationWindowObject);
-            }catch(System.Exception){
+                currentConfirmationWindowObject = Instantiate(confirmationWindowObject);
+            }
+            catch (Exception)
+            {
                 keyChangeInputs[currentKeyIndex].text = "err";
             }
         }
     }
 
-    void Update(){
-        KeyCode pauseKey = PlayerPrefs.HasKey("KeyPause") ? (KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("KeyPause")): KeyCode.Escape;
+    /// <summary>
+    /// Listens for the pause key to close the menu if no confirmation is open.
+    /// </summary>
+    void Update()
+    {
+        KeyCode pauseKey = PlayerPrefs.HasKey("KeyPause")
+            ? (KeyCode)Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("KeyPause"))
+            : KeyCode.Escape;
+
         if (!currentConfirmationWindowObject && Input.GetKeyDown(pauseKey))
         {
             Destroy(gameObject);
